b13afa58fa7de30d065535370dd9cbc682c4a47e
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-10-13 18:50:05 -0500

    added 'download-logs' task

---
+++ gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  var _ = grunt.util._;
+  var shell = require('shelljs');
+
+  var config = {};
+  var globalOptions = {
+    urls: 'etc/urls.json',
+    logsdir: 'logs'
+  };
+  var tasks = shell.ls('tasks').map(function(taskFile) {
+    var name = taskFile.split('.')[0];
+    var module = require('./tasks/' + name);
+
+    config[name] = _.extend({}, {options: globalOptions});
+
+    return {name: name, module: module, desc: module.desc};
+  });
+
+  grunt.initConfig(config);
+
+  tasks.forEach(function(task) {
+    grunt.registerTask(task.name, task.desc, task.module);
+  });
+};
---
+++ tasks/download-logs.js
@@ -0,0 +1,53 @@
+module.exports = function() {
+  var _ = require('lodash');
+  var shell = require('shelljs');
+  var request = require('request');
+  var options = this.options();
+  var urls = require('./../' + options.urls);
+  var taskFinished = this.async();
+  var fs = require('fs');
+  var async = require('async');
+
+  function fname(url) {
+    return options.logsdir + '/' + _.last(url.split('/'));
+  }
+
+  var q = async.queue(function(url, done) {
+    request(url, function(err, response) {
+      if (err) {
+        console.error('ERROR: could not get ' + url, err);
+        done(false);
+      }
+
+      fs.writeFile(fname(url), response.body, function(err) {
+        if (err) {
+          console.error('ERROR: could not write file "' + fname(url) + '"');
+          done(err);
+        }
+
+        done();
+      });
+    });
+  }, 20);
+
+  if (!shell.test('-e', options.logsdir)) {
+    shell.mkdir(options.logsdir);
+  }
+
+  urls.slice(0, 3).forEach(function(url) {
+    if (!shell.test('-f', fname(url))) {
+      console.log('fetching', url);
+      q.push(url);
+    } else {
+      console.log('skipping "' + url + '"');
+    }
+  });
+
+  if (q.length()) {
+    q.drain = taskFinished;
+  } else {
+    taskFinished();
+  }
+};
+
+module.exports.description = 'download the daily logs from @isaacs site';
