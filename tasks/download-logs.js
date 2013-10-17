393c0944f3d895403a51c6847cae6d84078503a4
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-10-17 10:26:25 -0500

    added another 6 days of logs

--- tasks/download-logs.js
+++ tasks/download-logs.js
@@ -34,7 +34,7 @@ module.exports = function() {
     shell.mkdir(options.logdir);
   }
 
-  urls.slice(0, 330).forEach(function(url) {
+  urls.slice(0, 340).forEach(function(url) {
     if (!shell.test('-f', fname(url))) {
       console.log('fetching', url);
       q.push(url);
